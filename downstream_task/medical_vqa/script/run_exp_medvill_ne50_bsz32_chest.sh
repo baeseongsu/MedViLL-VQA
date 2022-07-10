54d39c14cedaa49716eb7147ec0c29248c2652d3
baeseongsu <subae94@gmail.com>
2022-07-10 15:30:48 +0000

    rewrite codes for medical vqa task

---
+++ downstream_task/medical_vqa/script/run_exp_medvill_ne50_bsz32_chest.sh
@@ -0,0 +1,17 @@
+PATH_ITR=/home/dekyung/seongsu_temp/MedViLL-VQA/downstream_task/medical_vqa/pretrained_model/par/pytorch_model.bin
+EXP_NAME=medvill_ne50_bsz32_chest
+CUDA_VISIBLE_DEVICES=4 python -m torch.distributed.launch \
+--nproc_per_node=1 \
+--master_port 9872 \
+--use_env ./downstream_task/medical_vqa/finetune_modified.py \
+--model_recover_path ${PATH_ITR} \
+--tasks vqa \
+--s2s_prob 0 \
+--bi_prob 1 \
+--mask_prob 0 \
+--vqa_rad chest \
+--num_train_epochs 50 \
+--train_batch_size 32 \
+--exp_name=${EXP_NAME} \
+--wandb \
+--wandb_project phase1-qa-dataset-debugging-seongsu
